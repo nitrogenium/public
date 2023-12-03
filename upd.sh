a474ea44f56ce19abfc896eac692923d3348411a
nitrogen <liquid.nitrogenium@gmail.com>
2023-12-03 19:04:26 +0500

    upd

---
+++ upd.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Add Jammy repository to sources list
+sudo bash -c 'echo "deb http://cz.archive.ubuntu.com/ubuntu jammy main" >> /etc/apt/sources.list'
+
+# Add the PPA
+sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+
+# Update package lists
+sudo apt update
+
+# Install the required packages
+#sudo apt-get -o Dpkg::Options::="--force-confold" -y install g++-11
+#sudo apt-get -o Dpkg::Options::="--force-confold" -y install gcc-11
+sudo apt-get --yes --force-yes -o Dpkg::Options::="--force-confold" -q -y install libc6
+
+# Print done when finished
+message ok "Успешно обновлено"
